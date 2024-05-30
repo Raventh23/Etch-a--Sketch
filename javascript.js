a6fa48cb06e88dccf1c6d37d048bea0a0a31df1a
Raventh23 <blazenether@gmail.com>
2024-05-30 19:07:49 -0400

    Add code and css to make sketchboard added the code needed to create the 16 by 16 grid as well as the css to make the divs visable and into squares using flexbox

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
 let body = document.querySelector('body');
 
-body.createElement('div');
+let etchBoard = document.createElement('div'); //create the etch-a-sketch board
+etchBoard.setAttribute('id', 'etchBoard');
+body.appendChild(etchBoard);
+
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    etchBoard.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+        let cell = document.createElement('div');
+        cell.setAttribute('class', 'cell');
+        row.appendChild(cell);
+    }
+}

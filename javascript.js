08507158d0e68f1f7d989ceeb9b6d46a884c3962
Raventh23 <blazenether@gmail.com>
2024-05-31 18:55:10 -0400

    add button that allows for resizing of board add button css to stylesheet, add the create table code to a function

--- javascript.js
+++ javascript.js
@@ -4,17 +4,45 @@ let etchBoard = document.createElement('div'); //create the etch-a-sketch board
 etchBoard.setAttribute('id', 'etchBoard');
 body.appendChild(etchBoard);
 
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    etchBoard.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        let cell = document.createElement('div');
-        cell.setAttribute('class', 'cell');
-        row.appendChild(cell);
+let userButton = document.createElement('btn');
+userButton.setAttribute('id', 'userButton');
+userButton.addEventListener('click', ()=>
+    userTable());
+body.appendChild(userButton);
+
+function userTable() { //removes the table in place and replaces it with a table of the user selected size
+    let remove = document.querySelectorAll('.row');
+    remove.forEach((row) => {
+        row.remove();
+    })
+    let size = 0
+    while(size < 1 || size > 99){
+        size = prompt('pick a number from 1-99')
     }
+    createTable(size)
+    let cells = document.querySelectorAll('.cell'); //adds an event listner to each cell that turns blue when moused over
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover",() => {
+            cell.style.backgroundColor = 'blue';
+        });
+    });    
 }
-let cells = document.querySelectorAll('.cell');
+
+let size = 16 
+function createTable(size) { //creates the table at whatever size is set by the user or 16 on start
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        etchBoard.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement('div');
+            cell.setAttribute('class', 'cell');
+            row.appendChild(cell);
+        }
+    }}
+createTable(size)
+
+let cells = document.querySelectorAll('.cell'); //adds an event listner to each cell that turns blue when moused over
 cells.forEach((cell) => {
     cell.addEventListener("mouseover",() => {
         cell.style.backgroundColor = 'blue';
